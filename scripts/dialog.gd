6690a7552969174f373970adf38b0d9f2ee6307f
Anton Kryukov <anton_kryukov@epamneoris.com>
2026-01-31 20:34:45 +0100

    Refactor project

---
+++ scripts/dialog.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var dialogo: Panel = $dialogo
+@onready var screen: Panel = $screen
+@onready var facecam: Panel = $facecam
+
+func load_location(name: String)-> void:
+	pass
+	
---
+++ scripts/locations/controller.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@onready var target: Node = $target
+
+func load_location(name: String)-> void:
+	pass
