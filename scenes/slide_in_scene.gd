5ee23866a63672fc57414232bb00264f2f1427c3
LHEN RÍOS ROLDÁN <lhenrios.alu@iespacomolla.es>
2026-01-31 15:29:45 +0100

    escena location.atropello esqueleto + funcionalidad parcial

---
+++ scenes/slide_in_scene.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+# Expone la variable en el Inspector del editor
+var speed := 5.0 	# Velocidad de movimiento por pantalla
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since
+# the previous frame.
+func _process(delta: float) -> void:
+	pass
